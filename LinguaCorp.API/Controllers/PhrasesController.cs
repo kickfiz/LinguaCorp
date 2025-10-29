06078d463702513635de9e01e0fcc2f6cc2da876
André Vieira <andrefpvieira2@hotmail.com>
2025-10-29 14:38:43 +0000

    Build 1 complete

---
+++ LinguaCorp.API/Controllers/PhrasesController.cs
@@ -0,0 +1,33 @@
+﻿using LinguaCorp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinguaCorp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PhrasesController : ControllerBase
+    {
+        // In-memory list of phrases
+        private static List<Phrase> Phrases = new List<Phrase>
+        {
+            new Phrase { Id = 1, OriginalText = "Hello", Language = "en", TranslatedText = "Olá" },
+            new Phrase { Id = 2, OriginalText = "Thank you", Language = "en", TranslatedText = "Obrigado" },
+            new Phrase { Id = 3, OriginalText = "Good morning", Language = "en", TranslatedText = "Bom dia" }
+        };
+
+        // GET: api/phrases
+        [HttpGet]
+        public IActionResult GetAllPhrases()
+        {
+            return Ok(Phrases);
+        }
+
+        // GET: api/phrases/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetPhraseById(int id)
+        {
+            var phrase = Phrases.FirstOrDefault(p => p.Id == id);
+            return Ok(phrase);
+        }
+    }
+}
---
+++ LinguaCorp.API/Models/Phrase.cs
@@ -0,0 +1,10 @@
+﻿namespace LinguaCorp.API.Models
+{
+    public class Phrase
+    {
+        public int Id { get; set; }
+        public string OriginalText { get; set; }
+        public string Language { get; set; }
+        public string TranslatedText { get; set; }
+    }
+}
