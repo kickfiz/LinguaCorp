002641f2d2165d16451c110e7b724f72cf570c02
André Vieira <andrefpvieira2@hotmail.com>
2025-10-29 15:08:13 +0000

    Step 1/2/3/4

--- LinguaCorp.API/Program.cs
+++ LinguaCorp.API/Program.cs
@@ -1,15 +1,16 @@
+using LinguaCorp.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSingleton<IPhraseService, PhraseService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,5 +22,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Swagger redirect
+app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
 
 app.Run();
---
+++ LinguaCorp.API/Services/IPhraseService.cs
@@ -0,0 +1,11 @@
+﻿using LinguaCorp.API.Models;
+
+namespace LinguaCorp.API.Services
+{
+    public interface IPhraseService
+    {
+        public List<Phrase> GetAllPhrases();
+        public Phrase? GetPhraseById(int id);
+        public Phrase CreatePhrase(Phrase phrase);
+    }
+}
